2b2a199bfa8cbfefdf08fa98a22eee38158bdc82
msm <msm@tailcall.net>
2022-03-25 15:51:33 +0100

    Enable u2f for swaylock

--- yubikey.nix
+++ yubikey.nix
@@ -6,4 +6,9 @@
     environment.systemPackages = with pkgs; [
         yubioath-desktop
     ];
+
+    # enable the pam module
+    security.pam.u2f.enable = true;
+    security.pam.services.swaylock = {};
+    security.pam.u2f.control = "required";
 }
